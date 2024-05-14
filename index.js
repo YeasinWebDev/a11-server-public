644979deb2576055f76e41e66c5765552bbd1427
Yeasin <ar9485727@gmail.com>
2024-05-14 09:00:46 +0600

    jwt and vercel deploy

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(cors());
+//Must remove "/" from your production URL
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://a-11-skillify.web.app",
+      "https://a-11-skillify.firebaseapp.com/",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+app.use(cookieParser());
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+//localhost:5000 and localhost:5173 are treated as same site.  so sameSite value must be strict in development server.  in production sameSite will be none
+// in development server secure will false .  in production secure will be true
+
+
 
 app.listen(PORT);
 
@@ -29,10 +54,42 @@ const client = new MongoClient(uri, {
   },
 });
 
+// custom middleware
+const verifyTOken  = (req,res,next) =>{
+  const token = req?.cookies?.token
+  if(!token){
+    return res.send({massage: 'unauthorized access'})
+  }
+    jwt.verify(token ,process.env.ACCESS_TOKEN, (err, decoded) =>{
+      if(err){
+        return res.send({massage: 'unauthorized access'})
+      }
+      req.user = decoded
+      next()
+    })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    //creating Token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN,{expiresIn:'1h'});
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+    
+    //clearing Token
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
+
+
 
     const database = client.db("A11");
     // collection for AllCourses
@@ -44,7 +101,11 @@ async function run() {
       res.json(courses);
     });
 
-    app.get("/coursesByEmail", async (req, res) => {
+    app.get("/coursesByEmail",verifyTOken, async (req, res) => {
+      if(req.query.email !== req.user.email) {
+        return res.status(403).send({massage: 'forbidden access'})
+      }
+
       const email = req.query.email;
       const courses = await AllCoursesCollection.find({
         "provider.email": email,
@@ -52,7 +113,7 @@ async function run() {
       res.send(courses);
     });
 
-    app.get("/courses/:id", async (req, res) => {
+    app.get("/courses/:id",verifyTOken, async (req, res) => {
       const id = req.params.id;
       const course = await AllCoursesCollection.findOne({
         _id: new ObjectId(id),
@@ -110,7 +171,11 @@ async function run() {
       res.json(result);
     });
 
-    app.get("/booked_courses", async (req, res) => {
+    app.get("/booked_courses",verifyTOken, async (req, res) => {
+      if(req.query.email !== req.user.email) {
+        return res.status(403).send({massage: 'forbidden access'})
+      }
+
       const email = req.query.email;
       const booked_courses = await bookedCoursesCollection
         .find({ user_email: email })
